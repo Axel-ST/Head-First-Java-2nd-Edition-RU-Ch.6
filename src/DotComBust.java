b4994ce824b7d9861d004303639a92470166d641
Axel-ST <Kozlov.axe@gmail.com>
2022-09-09 19:26:12 +0700

    Create game class files

---
+++ src/DotComBust.java
@@ -0,0 +1,9 @@
+/*
+ * Created by Axel_ST on 09.09.2022
+ *
+ * Head First Java, 2nd Edition (RU) p. 178
+ */
+
+public class DotComBust {
+
+}
---
+++ src/GameHelper.java
@@ -0,0 +1,9 @@
+/*
+ * Created by Axel_ST on 09.09.2022
+ *
+ * Head First Java, 2nd Edition (RU) p. 182
+ */
+
+public class GameHelper {
+
+}
