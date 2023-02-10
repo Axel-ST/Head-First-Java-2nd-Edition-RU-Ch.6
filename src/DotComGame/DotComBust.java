f202780f948ce8961cf048babf78c9960a91071b
Axel-ST <Kozlov.axe@gmail.com>
2023-02-10 10:24:36 +0700

    Create DotComBust class file. Created fields and methods to further describe the work of.

--- src/DotComGame/DotComBust.java
+++ src/DotComGame/DotComBust.java
@@ -6,6 +6,33 @@
 
 package DotComGame;
 
-public class DotComBust {
+import java.util.ArrayList;
 
+public class DotComBust {
+    //starting initialization
+    private GameHelper helper = new GameHelper();
+    private ArrayList <DotCom> dotComsList = new ArrayList <DotCom>();
+    private int numOfGuesses = 0;
+    
+    // method for the initial creation of DotCom objects and provide a user manual.
+    private void setUpGame() {
+    }
+    
+    // gameplay method
+    private void startPlaying() {
+    }
+    
+    // hit test method
+    private void checkUserInput(String userGuess) {
+    }
+    
+    // method of displaying the game result
+    private void finishGame() {
+    }
+    
+    public static void main(String[] args) {
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
+    }
 }
